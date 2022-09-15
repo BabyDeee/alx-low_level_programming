99f4ddc32982640f75ebb6d7055549424c9e92ef
BabyDeee <francesamaka2002@gmail.com>
2022-09-15 14:35:06 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints the alphabet in lowercase
+ *
+ * Return: lways 0 (Success)
+ */
+
+int main(void)
+{
+	char lower_case = 'a';
+
+	for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
+	{
+		putchar(lower_case);
+	}
+		putchar('\n);
+		return (0);
+}
