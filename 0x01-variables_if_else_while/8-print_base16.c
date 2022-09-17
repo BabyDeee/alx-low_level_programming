6de3152a41b3843f1cf5978ba2fd9eb459f76855
BabyDeee <francesamaka2002@gmail.com>
2022-09-17 15:24:16 -0700

    9

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,7 +11,7 @@ int main(void)
 	int i;
 	char hexa;
 
-	for (i = 0; i <= 9; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
 		putchar(i);
 	}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints all possible combinations of
+ * a single digit number
+ *
+ * Return: A lways 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+
+		if (i != '9')
+		{
+		putchar(',');
+		putchar(' ');
+		}
+	}
+	return (0);
+}
