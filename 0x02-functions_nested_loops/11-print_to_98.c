7023e4f1e3338dea571846030f2de41f3b1bdc5c
BabyDeee <francesamaka2002@gmail.com>
2022-09-18 22:28:33 +0100

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,20 +9,20 @@
 
 void print_to_98(int n)
 {
-    while (n < 98)
-	{
-		printf("%d, ", n);
-		n++;
-	}
+while (n < 98)
+{
+printf("%d, ", n);
+n++;
+}
 
-	while (n > 98)
-	{
-		printf("%d, ", n);
-		n--;
-	}
-    if (n == 0)
-    {
-        printf("%d, ", n);
-    }
-    printf("\n");
+while (n > 98)
+{
+printf("%d, ", n);
+n--;
+}
+if (n == 0)
+{
+printf("%d, ", n);
+}
+printf("\n");
 }
