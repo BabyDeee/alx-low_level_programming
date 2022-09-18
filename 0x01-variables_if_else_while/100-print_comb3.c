2e097367fdbe969c7f464937ee8709618e97d0a4
BabyDeee <francesamaka2002@gmail.com>
2022-09-18 05:49:28 -0700

    100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,5 +8,26 @@
 
 int main(void)
 {
+	int first;
+	int second;
+
+	for (first = '0'; first <= '9'; first++)
+	{
+		for (second = '0'; second <= '9'; second++)
+		{
+			if (first < second)
+			{
+				putchar(first);
+				putchar(second);
+
+				if (first != '8' || second != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
 	return (0);
 }
