c7b5bdad1e8c4c48af9bc4f36de06a82307ccabe
BabyDeee <francesamaka2002@gmail.com>
2022-09-18 09:42:23 -0700

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times
+ *
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char small;
+
+	while (i <= 10)
+	{
+		small = 'a';
+
+		while (small <= 'z')
+		{
+			_putchar(small);
+			small++;
+		}
+		i++;
+		_putchar('\n');
+	}
+}
