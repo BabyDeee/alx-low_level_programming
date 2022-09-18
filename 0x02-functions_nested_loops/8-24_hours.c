2a55ac8f2674e1a434d79e607c38841ef0798c63
BabyDeee <francesamaka2002@gmail.com>
2022-09-18 20:53:15 +0100

    half and half

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
+ *
+ * Return: Always 0
+ */
+
+void jack_bauer(void)
+{
+    return (0);
+}
