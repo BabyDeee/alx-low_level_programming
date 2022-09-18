4ccaecfc06a815da80104d6cdc0d5009edfd6aed
BabyDeee <francesamaka2002@gmail.com>
2022-09-18 05:57:00 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char _putchar = _putchar;
+
+	printf("%c", _putchar);
+
+	return (0);
+}
