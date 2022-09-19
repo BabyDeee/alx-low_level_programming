d9d2e6d162e4e461c798f3b6446fd35d83e28ef4
Maduka Chiamaka <francesamaka2002@gmail.com>
2022-09-19 00:49:42 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* mul - multiplies two integers.
+* @a: first chracter to be multiplied
+* @b: second character to be multiplied
+*
+* Return: Always 0 (Success)
+*/
+
+int mul(int a, int b)
+{
+}
