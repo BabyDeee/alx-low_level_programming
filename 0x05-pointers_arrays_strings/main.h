10b7492e95988bfe757bea3e986bf3daffd50c46
Maduka Chiamaka <francesamaka2002@gmail.com>
2022-09-19 13:55:46 -0700

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,6 +5,22 @@ void reset_to_98(int *n); /* TASK 0 */
 
 void swap_int(int *a, int *b); /* TASK 1 */
 
-int _strlen(char *s); /* TASK 3 */
+int _strlen(char *s); /* TASK 2 */
+
+void _puts(char *str); /* TASK 3 */
+
+void print_rev(char *s);
+
+void rev_string(char *s);
+
+void puts2(char *str);
+
+void puts_half(char *str);
+
+void print_array(int *a, int n);
+
+char *_strcpy(char *dest, char *src);
+
+
 
 #endif
