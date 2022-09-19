f7c7b73b435925f856c3b816d73784677586d1eb
Maduka Chiamaka <francesamaka2002@gmail.com>
2022-09-19 01:22:33 -0700

    Update main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -14,4 +14,18 @@ int mul(int a, int b); /* Task 2 */
 
 void print_numbers(void); /* Task 3 */
 
+void print_most_numbers(void); /* Task 4 */
+
+void more_numbers(void); /* Task 5 */
+
+void print_line(int n); /* Task 6 */
+
+void print_diagonal(int n); /* Task 7 */
+
+void print_square(int size); /* Task 8 */
+
+void print_triangle(int size); /* Task 10 */
+
+
+
 #endif /* MAIN_H */
