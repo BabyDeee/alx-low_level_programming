abb788368688c9a9249f17d767282d86cb542007
Maduka Chiamaka <francesamaka2002@gmail.com>
2022-09-20 13:43:55 -0700

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,7 +3,7 @@
 /**
 * print_line - draws a straight line in the terminal.
 *@n: number of times the character _ should be printed
-
+*
 * Return: void
 */
 
@@ -20,4 +20,5 @@ void print_line(int n)
 	{
 		_putchar('\n');
 	}
+	_putchar('\n');
 }
